78b39a973fdcbe14e76eecaf0d3b50f4a2031ae9
SIDNEY SILVA <67187745+sxdny@users.noreply.github.com>
2024-01-19 18:21:32 +0100

    prueba con PUBLIC_

--- src/lib/spotify.js
+++ src/lib/spotify.js
@@ -1,8 +1,8 @@
 import querystring from 'querystring'
 
-const client_id = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
-const client_secret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
-const refresh_token = import.meta.env.VITE_SPOTIFY_REFRESH_TOKEN;
+const client_id = import.meta.env.PUBLIC_SPOTIFY_CLIENT_ID;
+const client_secret = import.meta.env.PUBLIC_SPOTIFY_CLIENT_SECRET;
+const refresh_token = import.meta.env.PUBLIC_SPOTIFY_REFRESH_TOKEN;
 
 const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64')
 const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`
