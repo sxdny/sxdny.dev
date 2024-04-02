ec856f7c1b941aa9ad9f852098eba8d29da83b7a
Sidney Silva Braz de Oliveira <silvasidney721@gmail.com>
2024-04-02 16:51:21 +0200

    feat: new home page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './src/**/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  './src/**/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  './src/**/**/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   prefix: '',
   theme: {
     container: {
